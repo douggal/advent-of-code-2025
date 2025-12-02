76851d45928fa90edd37a41805009029067f61f5
Doug <8922663+douggal@users.noreply.github.com>
2025-12-01 22:48:27 -0500

    Solve Day 1 Part 2

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -31,37 +31,43 @@ pub fn run() {
     // Debug:  Visually validate the puzzle input: Check for missing first and/or last row, etc!
     // dbg!(&input);
     // Debug:  Visually validate the puzzle input: Check for missing first and/or last row, etc!
-    println!("Raw input: {:?}", input);
-    println!("Input as Vec<String>:\n{}", input_vec.join("\n"));
-    println!("End input inspection\n");
-
+    #[cfg(debug_assertions)]
+    {
+        println!("Input file: {}", filename);
+        println!("Lines in input: {}", input_vec.len());
+        //println!("Raw input: {:?}", input);
+        println!("Input as Vec<String>:\n{}", input_vec.join("\n"));
+        println!("End input inspection\n");
+    }
 
     // Track program runtime by elapsed time shown by a "clock on the wall"
     let stop_watch = Instant::now();
+    let max:i32 = 100;  // count of clicks in full rotation
 
     //////////
     // Part 1
     //////////
 
-    // Iterate over the input vector of String
-    let max:i32 = 100;
+    // save the starting numeral and the numeral found at the end of each move
     let mut result_vec: Vec<i32> = vec![];
     result_vec.push(50);
+
+    // Iterate over the input vector of String
     for (row, line) in input_vec.iter().enumerate() {
-        let (direction, count) = line.split_at(1);
-        let count: i32 = count.parse().unwrap();
-        let count = count % max;
+        let (direction, clicks) = line.split_at(1);
+        let count_clicks: i32 = clicks.parse().unwrap();
+        let count_clicks = count_clicks % max;
 
         let dial_points_to = result_vec[result_vec.len() - 1];
         match direction {
             "R" => {
-                result_vec.push((dial_points_to + count) % max);
+                result_vec.push((dial_points_to + count_clicks) % max);
             }
             "L" => {
-                if (dial_points_to - count) >= 0 {
-                    result_vec.push(dial_points_to - count);
+                if (dial_points_to - count_clicks) >= 0 {
+                    result_vec.push(dial_points_to - count_clicks);
                 } else {
-                    result_vec.push(max - (dial_points_to - count).abs());
+                    result_vec.push(max - (dial_points_to - count_clicks).abs());
                 }
             }
             _ => eprintln!("Error on line {}", row),
@@ -79,10 +85,42 @@ pub fn run() {
     //////////
     // Part 2
     //////////
-    let answer_p2 = 0;
-    println!("Part 2. answer ... {answer_p2}");
+
+    // save the starting numeral and the numeral found at the end of each move
+    let mut result_vec: Vec<i32> = vec![];
+    result_vec.push(50);
+
+    // Iterate over the input vector of String
+    for (row, line) in input_vec.iter().enumerate() {
+        let (direction, clicks) = line.split_at(1);
+        let count_clicks: i32 = clicks.parse().unwrap();
+
+        // the dial points to the result of last rotation
+        let dial_points_to = result_vec[result_vec.len() - 1];
+        match direction {
+            "R" => {
+                for i in 1..=count_clicks {
+                    result_vec.push((dial_points_to + i) % max);
+                }
+            }
+            "L" => {
+                for i in 1..=count_clicks {
+                    let i_relative = i % max;
+                    if (dial_points_to - i_relative) >= 0 {
+                        result_vec.push(dial_points_to - i_relative);
+                    } else {
+                        result_vec.push(max - (dial_points_to - i_relative).abs());
+                    }
+                }
+            }
+            _ => eprintln!("Error on line {}", row),
+        }
+    }
+    // dbg!(&result_vec);
+    let answer_p2 = result_vec.iter().filter(|&&v| v == 0).count();
+    println!("Part 2 answer {}", answer_p2);
     println!("Elapsed time part 2: {:.2?}", stop_watch.elapsed()-lap1);
 
-    println!("\nTotal elapsed runtime: {:.2?}", stop_watch.elapsed());
+    println!("\nTotal elapsed runtime by 'clock on the wall' method: {:.2?}", stop_watch.elapsed());
 }
 
