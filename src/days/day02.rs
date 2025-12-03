5623e80881cec2fc9fe0028dd8cc8e81b8b7914b
Doug <8922663+douggal@users.noreply.github.com>
2025-12-03 02:55:11 -0500

    Solve Day 2 Part 2

--- src/days/day02.rs
+++ src/days/day02.rs
@@ -7,7 +7,7 @@ use std::time::Instant;
 // 02 December 2025
 ////////////////////////////////////////////////////////////////
 
-#[allow(dead_code)]
+// #[allow(dead_code)]
 #[allow(unused_variables)]
 pub fn run() {
     println!("AoC 2025 Day 2");
@@ -71,7 +71,6 @@ pub fn run() {
     // with test data result should be ...
     // assert_eq!(bad_id_numbers.iter().sum::<u64>(),1227775554);
 
-
     let answer_p1 = bad_id_numbers.iter().sum::<u64>();
     println!("Part 1 answer {}", answer_p1);
     let lap1 = stop_watch.elapsed();
@@ -81,9 +80,51 @@ pub fn run() {
     // Part 2
     //////////
 
+    let mut bad_id_numbers2:Vec<u64> = vec![];
+    for range_text in input_vec[0].split(",") {
+        let this_range = range_text.split("-").collect::<Vec<&str>>();
+        let range_start:u64 = this_range[0].parse().unwrap();
+        let range_end:u64 = this_range[1].parse().unwrap();
+        // println!("Ranges parsed and ready: {}..={}", range_start, range_end);
 
+        for i in range_start..=range_end {
+            let i_as_str = i.to_string();
+            let str_len = i_as_str.len();
 
-    let answer_p2 = 0;
+            let mut divisors:Vec<usize> = vec![];
+            for d in 1..=str_len {
+                if str_len.is_multiple_of(d) {
+                    divisors.push(d);
+                }
+            }
+            //println!("Divisors {}, {:?}", str_len, divisors);
+
+            for slice_size in divisors {
+                if str_len.is_multiple_of(slice_size as usize) {
+                    let mut slices:Vec<&str> = vec![];
+                    for i in (0..i_as_str.len()).step_by(slice_size as usize) {
+                        slices.push(&i_as_str[i..i+slice_size]);
+                    }
+                    // println!("slices: {:?}", slices);
+                    if slices.len() > 1 {
+                        let first = slices[0];
+                        let test = slices.iter().filter(|&s| *s == first).collect::<Vec<&&str>>();
+                        if test.len() == slices.len() {
+                            bad_id_numbers2.push(i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    bad_id_numbers2.sort_unstable();
+    bad_id_numbers2.dedup();
+
+    // with test data result should be ...
+    // assert_eq!(bad_id_numbers2.iter().sum::<u64>(),4174379265);
+
+    let answer_p2 = bad_id_numbers2.iter().sum::<u64>();
     println!("Part 2 answer {}", answer_p2);
     println!("Elapsed time part 2: {:.2?}", stop_watch.elapsed() - lap1);
 
